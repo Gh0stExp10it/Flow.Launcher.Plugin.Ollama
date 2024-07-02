bf42a17df7c30471d99dbbdd0f73a727f7e37147
Gh0stExp10it <lukas.erdt@gmx.de>
2024-07-02 18:29:52 +0200

    feat: initial setup (#7)
    * feat: initial setup
    
    * feat: initial setup
    
    * feat: initial setup
    
    * feat: initial setup
    
    * feat: initial setup

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
-import sys,os
+import sys
+import os
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
 
-from plugin.ollama import Ollama
+from plugin.main import Query
+from pyflowlauncher import Plugin
 
 if __name__ == "__main__":
-    Ollama()
+    plugin = Plugin()
+    plugin.add_method(Query(plugin))
+    plugin.run()
--- plugin/ollama.py
+++ plugin/ollama.py
@@ -1,44 +1,94 @@
-import sys
 import logging
-import requests
-from flox import Flox
+import pyperclip
 from ollama import Client as OllamaClient, ResponseError as OllamaResponseError
 
-class Ollama(Flox):
-    def __init__(self):
-        self.ollama_host = self.settings.get("ollama_host")
-        self.ollama_model = self.settings.get("ollama_model")
-        self.prompt_stop = self.settings.get("prompt_stop")
-        self.log_level = self.settings.get("log_level")
-        self.logger_level(self.log_level)
-        
-    def hello_world(self, flag: bool):
-        logging.critical(f"Hello World!")
-        logging.critical(f"Model: {self.ollama_model}")
-        
-        if flag:
-            return "Hello World!"
-        else:
-            return "Flag is not set!"
-           
-    def query(self, query: str) -> None:
-        if not self.ollama_host:
-            self.add_item(
-                title="Unable to load Ollama",
-                subtitle=(
-                    "Please enter a valid URL - e.g. http://localhost:11434"
-                ),
-            )
-            return
-        else:
-            self.add_item(
-                title="Result",
-                subtitle=f"Answer: {self.hw(True)}",
-                method=self.hw(True),
-                parameters=["answer"],
-            )
+class Ollama:
+    def __init__(self, ollama_host, ollama_model):
+        """
+        Initializes an Ollama client object.
 
-        return
+        Args:
+            * ollama_host (str): The hostname or IP address of the Ollama server.
+            * ollama_model (str): The name of the Ollama model to use for communication.
+        """
+        self.ollama_host = ollama_host
+        self.ollama_model = ollama_model
+        self.initialized = False
+            
+    def initialize(self):
+        """
+        Establishes a connection to the Ollama server.
 
-if __name__ == "__main__":
-    Ollama().run()
+        Args:
+            * self (object): Reference to the current object instance.
+        """
+        if not self.initialized:
+            # Initialize connection to InfluxDB
+            self.ollama_client = OllamaClient(host=self.ollama_host)
+            self.initialized = True
+            logging.info("Ollama-Client initialized.")
+        
+    def check_model(self, pull_model):
+        """
+        Checks if the specified Ollama model exists and attempts to download it if not.
+        
+        Args:
+            * self (object): Reference to the current object instance.
+            * pull_model (bool, optional): A flag indicating whether to automatically download the model if it's not found. Defaults to False.
+
+        Returns:
+            * bool: True if the model exists and can be communicated with, False otherwise.
+        """
+        self.initialize()
+        
+        try:
+            self.ollama_client.chat(self.ollama_model)
+            logging.info(f"LLM/Model '{self.ollama_model}' exists, no download required!")
+            return True
+        except OllamaResponseError as e:
+            logging.error(f"Error: <{e.status_code}> - {e.error}")
+            
+            if e.status_code == 404:
+                if pull_model:
+                    logging.info(f"The model '{self.ollama_model}' will now be downloaded automatically!!")
+                    self.ollama_client.pull(self.ollama_model)
+                    return True
+                else:
+                    logging.error(f"The model cannot be pulled if the automatic download has not been configured by the user!")
+                    return False
+            else:
+                return False
+    
+    def chat(self):
+        """
+        Sends the requested chat message to the Ollama model and retrieves the response.
+
+        Args:
+            * self (object): Reference to the current object instance.
+            * message (str, optional): The message to send to the model. Defaults to "Whats 1 plus 1?".
+
+        Returns:
+            * str: The response content from the model, or None if an error occurs.
+        """
+        try:
+            response = self.ollama_client.chat(model=self.ollama_model, messages=[
+                {
+                    'role': 'user',
+                    'content': 'Whats 1 plus 1?',
+                },
+            ])
+        
+            if isinstance(response, dict):
+                # logging.critical(f"RESPONSE: {response['message']['content']}")
+                return response['message']['content']
+        except OllamaResponseError as e:
+                logging.critical(f"Error: <{e.status_code}> - {e.error}")
+
+    def clipboard_copy(self, response: str) -> None:
+        """
+        Copies the provided response text to the clipboard.
+
+        Args:
+            * self (object): Reference to the current object instance.
+        """
+        pyperclip.copy(response)
