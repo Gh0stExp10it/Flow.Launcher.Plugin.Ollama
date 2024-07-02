3921bcaa88041e5d9bbb2b93ce7a4e9f9699218c
Gh0stExp10it <lukas.erdt@gmx.de>
2024-07-02 14:14:19 +0200

    Feat/init (#5)
    * feat: init
    
    * Update ollama.py
    
    * feat: override icon

--- plugin/ollama.py
+++ plugin/ollama.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import requests
 from flox import Flox
 from ollama import Client as OllamaClient, ResponseError as OllamaResponseError
 
@@ -11,9 +12,33 @@ class Ollama(Flox):
         self.log_level = self.settings.get("log_level")
         self.logger_level(self.log_level)
         
-    def hello_world(self):
+    def hello_world(self, flag: bool):
         logging.critical(f"Hello World!")
         logging.critical(f"Model: {self.ollama_model}")
+        
+        if flag:
+            return "Hello World!"
+        else:
+            return "Flag is not set!"
+           
+    def query(self, query: str) -> None:
+        if not self.ollama_host:
+            self.add_item(
+                title="Unable to load Ollama",
+                subtitle=(
+                    "Please enter a valid URL - e.g. http://localhost:11434"
+                ),
+            )
+            return
+        else:
+            self.add_item(
+                title="Result",
+                subtitle=f"Answer: {self.hw(True)}",
+                method=self.hw(True),
+                parameters=["answer"],
+            )
+
+        return
 
 if __name__ == "__main__":
-    Ollama()
+    Ollama().run()
