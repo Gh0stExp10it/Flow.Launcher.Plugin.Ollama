018ad2885ffb1271be6b1b4661c234b447469b3d
Gh0stExp10it <lukas.erdt@gmx.de>
2024-07-08 13:10:09 +0200

    feat: renamed class

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
-from plugin.main import Query
+from plugin.query import Query
 from pyflowlauncher import Plugin
 
 if __name__ == "__main__":
